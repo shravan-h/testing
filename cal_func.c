d21ad5e946bf7137d3dbf9c95e81c4915b02217f
shravan-h <48976788+shravan-h@users.noreply.github.com>
2019-03-28 14:29:25 +0530

    Create cal_func.c

---
+++ cal_func.c
@@ -0,0 +1,31 @@
+#include "cal.h"
+
+float calculator(float num1,char op, float num2)
+{
+    float result=0.0f;
+    switch(op)
+    {
+        case '+':
+            result = add(num1, num2);
+
+            break;
+
+        case '*':
+            result = mult(num1, num2);
+            break;
+
+
+        default:
+            printf("Invalid operator");
+    }
+    return result;
+}
+float add(float num1, float num2)
+{
+
+    return num1 + num2;
+}
+float mult(float num1, float num2)
+{
+    return num1 * num2;
+}
